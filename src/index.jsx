1214ff7d9c09f33a37aec9931aebf60e76333ece
Ken Inoue <worldken@Kens-MacBook-Air.local>
2019-05-01 11:10:43 -0500

    04-eslint-react

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+console.log("hola");
+
 ReactDOM.render(<h1>hola mundo!</h1>, document.getElementById("app"));
 
 module.hot.accept();
